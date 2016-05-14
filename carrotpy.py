1b12c51a3da599843d7cdb6c472823666295585a
Carrot37 <nathan@corkitt.org>
2016-05-14 11:55:26 -0700

    Add files via upload

---
+++ carrotpy.py
@@ -0,0 +1,36 @@
+def add(s):
+	s = s.split()
+	x = 0
+	for i in s:
+		x += float(i)
+	if int(x) == x:
+		return int(x)
+	else:
+		return x
+def sub(s):
+        s = s.split()
+        x = float(s[0])
+        for i in s[1:]:
+                x -= float(i)
+        if int(x) == x:
+                return int(x)
+        else:
+                return x
+def mul(s):
+        s = s.split()
+        x = 1
+        for i in s:
+                x *= float(i)
+        if int(x) == x:
+                return int(x)
+        else:
+                return x
+def div(s):
+        s = s.split()
+        x = float(s[0])
+        for i in s[1:]:
+                x /= float(i)
+        if int(x) == x:
+                return int(x)
+        else:
+                return x	
